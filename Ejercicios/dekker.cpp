c8ace36f88ba747a86ac177668db93fc8a6bc810
Ricardo Ruiz <ricardoruizfdez@gmail.com>
2023-11-22 15:43:24 +0100

    Inicio Seminario 3 OpenMPI

--- Ejercicios/dekker.cpp
+++ Ejercicios/dekker.cpp
@@ -22,7 +22,7 @@ void funcion_hebra_p1()
             c1 = 1;
             if (turno == 2)
             {
-                cout << "Turno 2 y P2 en SC. " << endl;
+                cout << "Turno 2 y P2 con intención entrar SC. " << endl;
                 while (turno == 2)
                     cout << ".......P1 espera ocupado......" << endl;
 
@@ -30,7 +30,7 @@ void funcion_hebra_p1()
                 cout << "P1 con intención de entrar en SC." << endl;
             }
         }
-        cout << "P1. Inicio Sección Crítica" << endl;
+        cout << "P1. Inicio Sección Crítica" << flush;
         x++;
         cout << "x= " << x << endl;
         turno = 2;
@@ -45,14 +45,14 @@ void funcion_hebra_p2()
     while (true)
     {
         cout << "P2 fuera de SC" << endl;
-        c2 = 0;
         cout << "P2 con intención de entrar en SC" << endl;
+        c2 = 0;
         while (c1 == 0)
         {
             c2 = 1;
             if (turno == 1) 
             {
-                cout << "       Turno 1 y P1 en SC. " << endl;
+                cout << "       Turno 1 y P1 con intención entrar SC. " << endl;
                 while (turno == 1)
                     cout << ".......P2 espera ocupado......" << endl;
                 c2 = 0;
@@ -65,7 +65,7 @@ void funcion_hebra_p2()
         turno = 1;
         cout << "       Cambio a Turno 1" << endl;
         c2 = 1;
-        cout << "       P2. Fin Sección Crítica" << flush;
+        cout << "       P2. Fin Sección Crítica" << endl;
     }
 }
 
